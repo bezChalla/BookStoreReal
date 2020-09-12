dce5c99a0f02df1fc724e989132b915abf428ff6
bezChalla <bchalla@gmu.edu>
2020-09-11 20:12:43 -0400

    Add project files.

---
+++ BookStoreReal/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStoreReal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+        public ActionResult Helloworld() 
+        {
+            return View();
+        }
+        public ActionResult  BookData()
+        {
+            var model = new
+            {
+                id = "1",
+                title = "The Hunger Games"
+            };
+            return new JsonResult() { Data = model, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+        }
+    }
+}
---
+++ BookStoreReal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookStoreReal.Startup))]
+namespace BookStoreReal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
